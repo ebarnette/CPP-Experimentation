a4d9e1065a8e4f96dd7df47d9efdfed24df585f0
Sam Osborne <osbornes@microsoft.com>
2022-08-03 15:27:53 -0700

    Add c++/winrt app

---
+++ CameraEffectSelector/CameraEffectSelector/MainPage.cpp
@@ -0,0 +1,54 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+
+using namespace winrt;
+using namespace winrt::Windows::Media::Capture;
+using namespace Windows::UI::Xaml;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+
+namespace winrt::CameraEffectSelector::implementation
+{
+    MediaCapture mediaCapture;
+
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+        InitializeCameraAsync();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    IAsyncAction MainPage::InitializeCameraAsync()
+    {
+        try
+        {
+            mediaCapture = MediaCapture();
+            co_await mediaCapture.InitializeAsync();
+        }
+        catch (winrt::hresult_error const& ex)
+        {
+            // No camera
+        }
+
+        try
+        {
+            CameraPreview().Source() = mediaCapture;
+            co_await mediaCapture.StartPreviewAsync();
+        }
+        catch (winrt::hresult_error const& ex)
+        {
+        }
+    }
+}
---
+++ CameraEffectSelector/CameraEffectSelector/MainPage.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainPage.g.h"
+
+namespace winrt::CameraEffectSelector::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        winrt::Windows::Foundation::IAsyncAction InitializeCameraAsync();
+    };
+}
+
+namespace winrt::CameraEffectSelector::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
